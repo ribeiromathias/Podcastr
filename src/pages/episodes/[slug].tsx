beca3d2f5a060e30c32d600e3f093146ee70f1d3
mathi <mathiaswrc@gmail.com>
2021-05-03 01:53:21 -0300

    added context and events

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -2,6 +2,7 @@ import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { GetStaticPaths, GetStaticProps } from 'next';
 
 import { api } from '../../services/api';
@@ -23,7 +24,6 @@ type Episode = {
 type EpisodeProps = {
   episode: Episode;
 }
-
 
 export default function Episode( { episode }: EpisodeProps ) {
   return (
@@ -57,14 +57,30 @@ export default function Episode( { episode }: EpisodeProps ) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 2,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+
+  const paths = data.map(episode => {
+    return {
+      params: {
+        slug: episode.id
+      }
+    }
+  })
+
   return {
-    paths: [],
+    paths,
     fallback: 'blocking'
   }
 }
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
-  const {slug} = ctx.params;
+  const { slug } = ctx.params;
   const { data } = await api.get(`/episodes/${slug}`)
 
   const episode = {
